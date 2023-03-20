a2cb902b0b89322511c303371585a8f789161f77
Peter Nordel <peternordel@gmail.com>
2023-03-20 15:14:11 -0600

    complete lab

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let num3 = 14;
 let num4 = 5;
 
 const multiply = num1 * num2;
-const random = Math.floor(Math.random()*10);
+const random = Math.ceil(Math.random()*10);
 const mod = num3 % num4;
 const max = Math.max(1,5,10,20);
